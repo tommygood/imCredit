169547d612623a2a9e188b300fc6cf553295bea9
root <tommy50508@gmail.com>
2023-07-06 10:06:44 +0800

    update 3D數位內容與應用, add annotation in free.html

--- credit/settings.py
+++ credit/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-71jav%wr-zw3-!lsv3#%@ty%^%71rxwo4zwu4%d_m@66ze%@y_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["grade.im.ncnu.edu.tw"]
 
 
 # Application definition
